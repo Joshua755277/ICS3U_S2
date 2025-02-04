8253cee6a28d9fd77350e1a62c2536bc398145d8
Joshua755277 <755277@pdsb.net>
2025-02-04 12:12:24 -0500

    U1 L1.py

--- U1 L1.py
+++ U1 L1.py
@@ -3,4 +3,17 @@ name = "Leslie"
 print("Hello", name)
 
 # Predict: 
-# I think the output will print Hellow Leslie
+# I think the output will print Hello Leslie
+
+# Run:
+# My prediction is correct
+
+# Inspect:
+# The output is what I expected and there was no comma in the output since it was not included in the quotation mark or string and the seperator in the print statment is a space. In order to add a comma, you would need to include a comma in the quotations of "Hello". This would look like  print("Hello,", name)
+
+# Modify 
+# this is a code comment
+name = "Joshua"
+print("Hello", name)
+# This would print out "Hello Joshua"
+# The variable name is a string that holds the entered name and is used to print Hello, t
