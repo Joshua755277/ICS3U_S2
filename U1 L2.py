23cf561df81aa4a84da00abd66be2ca29a8fc7e6
Joshua755277 <755277@pdsb.net>
2025-02-05 13:55:59 -0500

    U1 L2.py

--- U1 L2.py
+++ U1 L2.py
@@ -1,6 +1,40 @@
 # Predict: 
 # I predict the code will output 24
+
 # Run:
 # My prediction was correct and it printed out 24
+
 # Inspect:
-# 
+# The output was 24 since the number 24 was put in a print statment
+
+# Modify:
+amount = 24
+print(amount)
+print("Two dozen is: ", amount)
+# This is an improvment since you are able to change the value it prints out when you change the number assigned to the variable "amount"
+
+# Modify 2  
+amount = 7 * 4
+print("forty-two is: ", amount)
+# The fixed way to print a correct statement would be 
+amount = 7 * 4
+print("twenty-eight is: ", amount)
+
+# Modify 3
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+# First it is setting the variable amount to 8 then in the next line it is adding 1 to amount making it 8 + 1 which is equal to 9
+amount = 8
+amount += 1 
+print("The amount is: ", amount)
+# amount += 1 is the same as amount = amount + 1
+
+# Modify 4
+amount = 8
+amount = amount * 2
+print(amount)
+# amount = amount * 2 would take the value of amount and double it since you are mulitplying by 2
+amount = 8
+amount *= 2
+print(amount)
