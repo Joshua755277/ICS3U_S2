9b8f5bad8280fe2bb255dfc4d3bb3b100a644d7e
Joshua755277 <755277@pdsb.net>
2025-02-05 14:15:27 -0500

    U1 L3.py

--- U1 L3.py
+++ U1 L3.py
@@ -1,1 +1,14 @@
 # Task A
+# I think that it will output:
+# Welcome to Sanjay's Koala Party!
+# It's possible Sanjay can distribute  12
+#  cookies for each bowl.
+# My prediction was correct 
+# I fixed the program to only print one space after the word distribute by removing the space after the word distribute within the quotation marks
+
+# Task B
+print("Sanjay has", 6, "bowls for", 144, "cookies")
+
+# Task C
+print("The number of cookies per bowl is", 12*12)
+# There is one text value and one number value (the 2 number values are being multipled together with * operation)
