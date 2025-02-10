a42827bb879cb4e73615deb63116d471e2200219
Joshua755277 <755277@pdsb.net>
2025-02-10 12:45:31 -0500

    U1 L7.py

--- U1 L7.py
+++ U1 L7.py
@@ -8,13 +8,13 @@
 # Inspect
 # Yes it did match the output of my calculator when I tried dividing x by y
 
-# Modify 1/2
+# Modify 1 and 2
 
 import math
 
 x = input("Please input a whole number: ")
 x = int(x)
-y = input("Please input another nonzero whole number")
+y = input("Please input another nonzero whole number: ")
 y = int(y)
 if (y != 0):
   
@@ -27,9 +27,25 @@ if (y != 0):
       print("No!", y, "is not a factor of", x)
   
 else:
-  quit()
+  print("Please do not enter 0")
+  print("A factor could not be determined")
 
-#Modify 2
+#Modify 3
+import math
+x = int(input("Please enter a number between 1 and 20: "))
+if ( 1 <= x <= 20):
+    y = int(input("Please enter a number between 1 and 20 again: "))
+    if ( 1 <= y <= 20):
+        print("Now deciding if", y, "is a factor of", x, "...")
+        rem = x % y
+        if rem == 0:
+          print("Yes!", y, "is a factor of", x)
+        else:
+          print(y, "is not a factor of", x)
+        
+    else: print(y, "is out of range. Cannot continue")
+else: 
+  print(x, "is out of range. Cannot continue")
   
   
 
