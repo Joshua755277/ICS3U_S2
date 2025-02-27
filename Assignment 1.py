378a7a481da60e4968955619bea2a45dbb1089b5
Joshua755277 <755277@pdsb.net>
2025-02-27 14:24:47 -0500

    Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -22,12 +22,13 @@ print("I will find the cube's inner diagonal for any edge length!")
 
 a = int(input("Please enter the edge length of your cube:"))
 d = a * math.sqrt(3)
+d = round(d, 2)
 
-print("The length of the inner diagonal of a cube with side length 8 is: %.2f" % d)
+print("The length of the inner diagonal of a cube with side length 8 is:", d)
 
 # Part 3: Making change in coins
 
-
+dollars = 0
 quarters = 0
 dimes = 0
 nickles = 0
@@ -37,40 +38,54 @@ change = int(input("Please enter the amount of change in cents:"))
 total = change
 
 if (change >= 100):
+  dollars = int(change/100)
   change = change % 100
-  
-  if (change >= 25):
-    quarters = change/25
-    change = change % 25
     
-  if (change >= 10):
-    dimes = change/10
-    change = change % 10
-      
-  if (change >= 5):
-    nickles = change/5
-    change = change % 5
-        
-  if (change>= 1):
-    pennies = change /1
-  
-  print("%.0f cents can be %.0f quarters, %.0f dimes, %.0f nickles, and %.0f pennies." % (total, quarters, dimes, nickles, pennies))
-            
-else:
-  
-  if (change >= 25):
-    quarters = change/25
-    change = change % 25
+if (change >= 25):
+  quarters = int(change/25)
+  change = change % 25
     
-  if (change >= 10):
-    dimes = change/10
-    change = change % 10
+if (change >= 10):
+  dimes = int(change/10)
+  change = change % 10
       
-  if (change >= 5):
-    nickles = change/5
-    change = change % 5
+if (change >= 5):
+  nickles = int(change/5)
+  change = change % 5
         
-  if (change>= 1):
-    pennies = change /1
+if (change>= 1):
+  pennies = int(change /1)
+    
+print(total, "can be ", end="")
   
-  print("%.0f cents can be %.0f quarters, %.0f dimes, %.0f nickles, and %.0f pennies." % (total, quarters, dimes, nickles, pennies))
+if dollars > 0:
+  if dollars > 1:
+    print(dollars, "dollars ", end="")
+  else:
+    print(dollars, "dollar ", end="")
+    
+if quarters > 0:
+  if quarters > 1:
+    print(quarters, "quarters ", end="")
+  else:
+    print(quarters, "quarter ", end="")
+  
+if dimes > 0:
+  if dimes > 1:
+    print(dimes, "dimes ", end="")
+  else:
+    print(dimes, "dime ", end="")
+    
+if nickles > 0:
+  if nickles > 1:
+    print(nickles, "nickles ", end="")
+  else:
+    print(nickles, "nickle ", end="")
+    
+if pennies > 0:
+  if pennies > 1:
+    print(pennies, "pennies.")
+  else:
+    print(pennies, "penny. ")
+
+
