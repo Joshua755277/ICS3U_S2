76e17775fa7216530246af765c56d82a7d9d9424
Joshua755277 <755277@pdsb.net>
2025-03-03 13:22:54 -0500

    U2 L1.py

---
+++ U2 L1.py
@@ -0,0 +1,3 @@
+# Predict
+# I predict that the output when you input C, would be "I prefer cherries"
+# If you were to input any other letter or symbol that is not "A", "B", or "C", it will still output "I prefer cherries" since
