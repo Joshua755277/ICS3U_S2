483a5040ac5e941d3be51d0542e2a00b0c7ba776
Joshua755277 <755277@pdsb.net>
2025-03-03 13:44:49 -0500

    U2 L2.py

--- U2 L2.py
+++ U2 L2.py
@@ -1,1 +1,22 @@
+# Predict
+# I think the code will output:
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4 
+# 3 
+# 2
+# 1
 
+# My prediction was correct
+
+# Modify 1
+
+count = 9
+while (count != 0):
+    count -= 1
+    print(count)
+
+# The output is different and it will start from 8 to 0 instead of 9 to 1
