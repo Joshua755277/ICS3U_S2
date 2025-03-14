b4f31877bdbd0df1d33ac314ac0e19952dd28c8c
Joshua755277 <755277@pdsb.net>
2025-03-13 23:02:11 -0400

    U2 L3.py

--- U2 L3.py
+++ U2 L3.py
@@ -1,1 +1,42 @@
+# Part A
+
+# 1 
+
+n = int(input("Please enter a number: "))
+count = n
+fac = 1
+
+while count > 0:
+  
+    fac *= count
+    count -= 1
+    
+print(n, "factorial is equal to", fac)
+
+# 2
+
+n = int(input("Please enter a number: "))
+count = n
+
+print(n, "factorial is equal to ", end="")
+
+while count > 0:
+  if count == 1:
+    print("1")
+    quit()
+  print(count, "x", end=" ")
+  count -= 1
+
+# 3
+
+n = int(input("Please enter a number: "))
+count = n
+fac = 1
+
+while count > 0:
+  
+    fac *= count
+    count -= 1
+    
+print(n, "factorial is equal to", fac)
 
