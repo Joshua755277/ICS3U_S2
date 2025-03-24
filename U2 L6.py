b24f68ebb9bc62dbdafc6d3bd962c8ca2f7864ae
Joshua755277 <755277@pdsb.net>
2025-03-24 12:05:51 -0400

    U2 L6.py

---
+++ U2 L6.py
@@ -0,0 +1,9 @@
+import math
+
+print("%3s|%5s|%7s|%5s" % ("N", "SQR", "Cubes", "Roots"))
+print("---+-----+-------+-----")
+for n in range (10, 200, 15):
+  sqr = n**2
+  cubes = n**3
+  root = round(math.sqrt(n), 2)
+  print("%3s|%5s|%7s|%5s" % (n, sqr, cubes, root))
