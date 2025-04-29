dce0c96818a959e34b95fb63eff1911fb7718070
Joshua755277 <755277@pdsb.net>
2025-04-29 14:17:04 -0400

    U3 L6.py

--- U3 L6.py
+++ U3 L6.py
@@ -1,1 +1,37 @@
+# Make
+def factorize(n):
+  array = []
+  for i in range (1, n):
+    if n % i == 0:
+      array.append(i)
+  return array
+  
+print(factorize(50))
 
+# Make More
+
+n = int(input("Please enter a number: "))
+
+def factorize(n):
+  array = []
+  for i in range (1, n):
+    if n % i == 0:
+      array.append(i)
+  return array
+
+array = factorize(n)
+
+def total(n):
+  sum = 0
+  for i in array:
+    sum += i
+  return sum
+
+print("Factor sum:", total(array))
+
+if (total(array) == n):
+  print(n, "is perfect")
+elif (total(array) > n):
+  print(n, "is abundant")
+else:
+  print(n, "is deficient")
